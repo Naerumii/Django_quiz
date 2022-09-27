f980ba6f4006745dad379ceaaab05bd1ad327a30
Naerumii <dpskfl04@gmail.com>
2022-09-27 15:31:26 +0900

    페이지 접속 로그 남기는 모델 생성

--- introduce/models.py
+++ introduce/models.py
@@ -7,3 +7,6 @@ class AccessLog(models.Model):
     # 3. auto_now_add :  데이터가 생성 될 때 시간을 기록
     created_at = models.DateTimeField("접속 시간", auto_now_add=True)
     location = models.CharField("접속 경로", max_length=50)
+    
+    def __str__(self):
+        return f"{self.created_at} / {self.location}"
